bd11285b0bb25a11ada1af50cd05022ed9eac42e
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-11-17 20:49:24 +0200

    update

---
+++ Lab 3/Functions.c
@@ -0,0 +1,52 @@
+// Lab 3 Functions
+/*
+
+page directory pointer (array) :
+
+type : uint32*
+name : ptr_page_directory
+notes : this pointer is declared in FOS . you can use it (ptr_page_directory)
+===================================================================================
+page table pointer (array) :
+
+type : uint32*
+implement :
+	uint32 *ptr_page_table = NULL;
+	int ret = get_page_table(ptr_page_directory, KERNEL_BASE, &ptr_page_table);
+	if (ret ==  TABLE_IN_MEMORY)   or    if (ptr_page_table != NULL)
+	{
+		cprintf("page table found at virtual address:%x\n", ptr_page_table);
+	}
+===================================================================================
+create page table :
+
+implement :
+	create_page_table(ptr_page_directory , VA) ;
+
+===================================================================================
+access page directory entry :
+
+	type : uint32
+	arr_name = ptr_page_directory
+	index :
+		type : uint32
+		name : PDX(VA)
+	page directory entry = ptr_page_directory[PDX(VA)]
+
+
+===================================================================================
+access page table entry :
+	type : uint32
+	arr_name = ptr_page_table
+	index :
+		type : uint32
+		name : PTX(VA)
+	page table entry = ptr_page_table [PTX(VA)]
+
+===================================================================================
+Frames :
+	type : int 
+	frameNum of page table = directory_entry >> 12 
+	frameNum of page = table_entry >> 12 
+
+===================================================================================
